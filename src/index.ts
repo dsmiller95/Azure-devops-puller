c626dc6362bf58a5b85ade4d4a5402f4ca1f6ed6
Dan Miller <dsmiller95@gmail.com>
2020-02-01 14:53:15 -0600

    added new summary object for PRs based on age

--- src/index.ts
+++ src/index.ts
@@ -5,15 +5,25 @@ import { ConnectionData } from 'azure-devops-node-api/interfaces/LocationsInterf
 
 interface ReturnType {
     success: boolean;
-    prs?: GitPullRequest[];
+    prs?: PullRequestSummary[];
     token?: string;
     connection?: ConnectionData;
+    hasNewPr?: boolean;
+}
+
+interface PullRequestSummary {
+    age: number;
+    description: string;
+    title: string;
 }
 
 const repositoryID = '654d2cf2-fe1f-4f47-9eee-4a92f00c2174';
+const orgUrl = "https://dev.azure.com/symplr";
+const projectName = "Provider Management";
+
+const maxAgeOfNewPr: number = 1 * 60 * 60 * 1000;
+
 exports.handler = async (event: any): Promise<ReturnType> => {
-    const orgUrl = "https://dev.azure.com/symplr";
-    const projectName = "Provider Management";
 
     let token = process.env.AZURE_PERSONAL_ACCESS_TOKEN;
     if(!token){
@@ -24,15 +34,19 @@ exports.handler = async (event: any): Promise<ReturnType> => {
     
     let connection = new azureDevApi.WebApi(orgUrl, authHandler);
 
-    const result = await connection.connect();
     const gitApi = await connection.getGitApi();
-    const prs = await gitApi.getPullRequests(repositoryID, {
+    const prs: GitPullRequest[] = await gitApi.getPullRequests(repositoryID, {
         repositoryId: repositoryID
     }, projectName);
-    
+
+    const currentTime = (new Date()).valueOf();
+    const prsByAgeAscending: PullRequestSummary[] = prs
+        .map(pr => ({age: currentTime - (pr.creationDate?.valueOf() ?? 0), title: pr.title ?? '', description: pr.description ?? ''}) )
+        .sort((prA, prB) => prA.age - prB.age);
+
     return {
         success: true,
-        prs,
-        connection: result
+        hasNewPr: prsByAgeAscending[0].age < maxAgeOfNewPr,
+        prs: prsByAgeAscending
     };
 };
