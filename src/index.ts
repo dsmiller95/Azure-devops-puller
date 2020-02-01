ba8ed89cc35276294b37c394c642e8ba2a153d78
Dan Miller <dsmiller95@gmail.com>
2020-02-01 14:33:49 -0600

    got all the configs set up to be able to pull relevant PR data

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,19 @@
 import fetch from 'node-fetch';
 import * as azureDevApi from "azure-devops-node-api";
 import { GitPullRequest } from 'azure-devops-node-api/interfaces/GitInterfaces';
+import { ConnectionData } from 'azure-devops-node-api/interfaces/LocationsInterfaces';
 
 interface ReturnType {
     success: boolean;
     prs?: GitPullRequest[];
+    token?: string;
+    connection?: ConnectionData;
 }
 
 const repositoryID = '654d2cf2-fe1f-4f47-9eee-4a92f00c2174';
 exports.handler = async (event: any): Promise<ReturnType> => {
-    let orgUrl = "https://symplr.visualstudio.com/Provider%20Management";
+    const orgUrl = "https://dev.azure.com/symplr";
+    const projectName = "Provider Management";
 
     let token = process.env.AZURE_PERSONAL_ACCESS_TOKEN;
     if(!token){
@@ -17,15 +21,18 @@ exports.handler = async (event: any): Promise<ReturnType> => {
     }
 
     let authHandler = azureDevApi.getPersonalAccessTokenHandler(token); 
+    
     let connection = new azureDevApi.WebApi(orgUrl, authHandler);
 
+    const result = await connection.connect();
     const gitApi = await connection.getGitApi();
     const prs = await gitApi.getPullRequests(repositoryID, {
         repositoryId: repositoryID
-    })
+    }, projectName);
     
     return {
         success: true,
-        prs
+        prs,
+        connection: result
     };
 };
