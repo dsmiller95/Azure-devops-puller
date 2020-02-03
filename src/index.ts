76f8e5fc083004533413eb064d3b018e11e52794
Dan Miller <dsmiller95@gmail.com>
2020-02-03 00:10:35 -0600

    pulled in iot library and publish a message to a topic based on if there are any new PRs

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import * as azureDevApi from "azure-devops-node-api";
 import { GitPullRequest } from 'azure-devops-node-api/interfaces/GitInterfaces';
 import { ConnectionData } from 'azure-devops-node-api/interfaces/LocationsInterfaces';
 
+import { IotData } from 'aws-sdk';
+
 interface ReturnType {
     success: boolean;
     prs?: PullRequestSummary[];
@@ -21,7 +23,8 @@ const repositoryID = '654d2cf2-fe1f-4f47-9eee-4a92f00c2174';
 const orgUrl = "https://dev.azure.com/symplr";
 const projectName = "Provider Management";
 
-const maxAgeOfNewPr: number = 1 * 60 * 60 * 1000;
+// if there was a PR created in the last 10 minutes, it is new
+const maxAgeOfNewPr: number = 10 * 60 * 1000;
 
 exports.handler = async (event: any): Promise<ReturnType> => {
 
@@ -31,7 +34,6 @@ exports.handler = async (event: any): Promise<ReturnType> => {
     }
 
     let authHandler = azureDevApi.getPersonalAccessTokenHandler(token); 
-    
     let connection = new azureDevApi.WebApi(orgUrl, authHandler);
 
     const gitApi = await connection.getGitApi();
@@ -44,9 +46,26 @@ exports.handler = async (event: any): Promise<ReturnType> => {
         .map(pr => ({age: currentTime - (pr.creationDate?.valueOf() ?? 0), title: pr.title ?? '', description: pr.description ?? ''}) )
         .sort((prA, prB) => prA.age - prB.age);
 
+    const hasNewPr = prsByAgeAscending[0].age < maxAgeOfNewPr;
+    await transmitResultToIot(hasNewPr);
+
     return {
         success: true,
-        hasNewPr: prsByAgeAscending[0].age < maxAgeOfNewPr,
+        hasNewPr,
         prs: prsByAgeAscending
     };
 };
+
+async function transmitResultToIot(result: boolean): Promise<any>{
+    let endpoint: string = process.env.IOT_ENDPOINT ?? '';
+    const iotData = new IotData({apiVersion: '2015-05-28', endpoint});
+    let params: IotData.PublishRequest = {
+        topic: 'ACswitch/switch', /* required */
+        payload: result ? 'true' : 'false'  /* Strings will be Base-64 encoded on your behalf */,
+        qos: 1
+    };
+
+    iotData.publish(params);
+
+    return null;// published.$response.data;
+}
