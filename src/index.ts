18181bdfd4ef743d95a2ec2c8233cac9f3554446
Dan Miller <dsmiller95@gmail.com>
2020-02-04 10:07:48 -0600

    implemented different pulses based on team identity

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import fetch from 'node-fetch';
 import * as azureDevApi from "azure-devops-node-api";
-import { GitPullRequest } from 'azure-devops-node-api/interfaces/GitInterfaces';
+import { WebApi } from "azure-devops-node-api";
+import { GitPullRequest, PullRequestStatus, GitPullRequestSearchCriteria } from 'azure-devops-node-api/interfaces/GitInterfaces';
 import { ConnectionData } from 'azure-devops-node-api/interfaces/LocationsInterfaces';
 
 import { IotData } from 'aws-sdk';
@@ -9,7 +10,7 @@ interface ReturnType {
     success: boolean;
     prs?: PullRequestSummary[];
     token?: string;
-    connection?: ConnectionData;
+    patternTransmitted?: PulseDefinition | null;
     hasNewPr?: boolean;
 }
 
@@ -17,6 +18,15 @@ interface PullRequestSummary {
     age: number;
     description: string;
     title: string;
+    author: string;
+}
+function summarizePr(pullRequest: GitPullRequest, currentTimeMillis: number): PullRequestSummary {
+    return {
+        age: currentTimeMillis - (pullRequest.creationDate?.valueOf() ?? 0),
+        title: pullRequest.title ?? '',
+        description: pullRequest.description ?? '',
+        author: pullRequest.createdBy?.displayName ?? ''
+    }
 }
 
 interface PulseDefinition {
@@ -26,59 +36,122 @@ interface PulseDefinition {
 
 interface PulseMap {
     newPr: PulseDefinition;
+    newTeamPr: {[team: string]: PulseDefinition};
 }
 
 const repositoryID = '654d2cf2-fe1f-4f47-9eee-4a92f00c2174';
 const orgUrl = "https://dev.azure.com/symplr";
 const projectName = "Provider Management";
 
+const teamDef: {[team: string]: string[]} = {
+    "Danakil": ["Andrew Donahoe", "Kelly O'Callaghan", "Josh Boyce", "Shane Cook", "James Pemberton"],
+    "Arctic": ["Andrew Wied", "Wade Carlson", "Duane Raiche", "Tom Duex", "Brian Kapellusch"],
+    "Mojave": ["Dan Miller", "Katie Lawton", "Tanner Irons", "Todd Schmitt"]
+}
+
 async function handler(event: any): Promise<ReturnType> {
-    const maxAgeOfNewPr: number = parseInt(process.env.NEW_PR_MINUTE_THRESHOLD ?? '10') * 60 * 1000;
-
-    let pulseMapping: PulseMap;
-    if(process.env.PULSE_RULE_MAP){
-        pulseMapping = JSON.parse(process.env.PULSE_RULE_MAP);
-    } else {
-        pulseMapping = {
-            newPr: {
-                pattern: 'XXXX----XX--X',
-                interval: 200
-            }
-        }
-    }
-
     let token = process.env.AZURE_PERSONAL_ACCESS_TOKEN;
     if(!token){
         return {success: false};
     }
 
-    let authHandler = azureDevApi.getPersonalAccessTokenHandler(token); 
-    let connection = new azureDevApi.WebApi(orgUrl, authHandler);
+    const pulsePattern = await getPulsePattern(
+        token,
+        parseInt(process.env.NEW_PR_MINUTE_THRESHOLD ?? '10'),
+        process.env.PULSE_RULE_MAP ? JSON.parse(process.env.PULSE_RULE_MAP) : undefined);
 
-    const gitApi = await connection.getGitApi();
-    const prs: GitPullRequest[] = await gitApi.getPullRequests(repositoryID, {
-        repositoryId: repositoryID
-    }, projectName);
-
-    const currentTime = (new Date()).valueOf();
-    const prsByAgeAscending: PullRequestSummary[] = prs
-        .map(pr => ({age: currentTime - (pr.creationDate?.valueOf() ?? 0), title: pr.title ?? '', description: pr.description ?? ''}) )
-        .sort((prA, prB) => prA.age - prB.age);
-
-    const hasNewPr = prsByAgeAscending[0].age < maxAgeOfNewPr;
-    const iotData = new IotData({apiVersion: '2015-05-28', endpoint: process.env.IOT_ENDPOINT ?? ''});
-
-
-    if(hasNewPr){
-        await transmitPulseToIot(pulseMapping.newPr, iotData);
+    if(!!pulsePattern) {
+        const iotData = new IotData({apiVersion: '2015-05-28', endpoint: process.env.IOT_ENDPOINT ?? ''});
+        await transmitPulseToIot(pulsePattern, iotData);
     }
-    ///await transmitResultToIot(hasNewPr, iotData);
 
     return {
         success: true,
-        hasNewPr
+        patternTransmitted: pulsePattern
     };
 };
+
+async function getPulsePattern(
+    azureToken: string,
+    prAgeThresholdMinutes: number,
+    pulseMap?: PulseMap): Promise<PulseDefinition | null>{
+    const maxAgeOfNewPr: number = prAgeThresholdMinutes * 60 * 1000;
+
+    const pulseMapping: PulseMap = pulseMap ?? {
+            newPr: {
+                pattern: 'XXXX----XX--X',
+                interval: 200
+            },
+            newTeamPr: {
+                default: {
+                    pattern: 'XX--X',
+                    interval: 200
+                },
+                Danakil: {
+                    pattern: 'X--XX--XX',
+                    interval: 200
+                },
+                Arctic: {
+                    pattern: 'XX--X--XX',
+                    interval: 200
+                },
+                Mojave: {
+                    pattern: 'XX--XX--X',
+                    interval: 200
+                },
+            }
+        };
+
+    let azureApiConnection = await getAzureApiConnection(azureToken);
+    let prData = await getPrData(azureApiConnection, maxAgeOfNewPr);
+
+    if(!prData.newPr){
+        return null;
+    }
+    const team = getTeamForPr(prData.newPr, teamDef);
+    return pulseMapping.newTeamPr[team] ?? pulseMapping.newPr;
+}
+
+function getTeamForPr(prSummary: PullRequestSummary, authorMap: {[team: string]: string[]}): string{
+    for(let team in authorMap){
+        if(authorMap[team].indexOf(prSummary.author) >= 0){
+            return team;
+        }
+    }
+    return "default";
+}
+
+async function getAzureApiConnection(token: string): Promise<WebApi> {
+    let authHandler = azureDevApi.getPersonalAccessTokenHandler(token); 
+    return new WebApi(orgUrl, authHandler);
+}
+
+async function getPrData(
+    connection: WebApi,
+    newPrThreshold: number,
+    additionalRequestOptions?: Partial<GitPullRequestSearchCriteria>) : Promise<{prs: PullRequestSummary[], newPr: PullRequestSummary | null}> {
+    const gitApi = await connection.getGitApi();
+    let searchCriteria = additionalRequestOptions ?? {};
+    const prs: GitPullRequest[] = await gitApi.getPullRequests(repositoryID, {
+        ...searchCriteria,
+        repositoryId: repositoryID,
+    },
+    projectName,
+    0);
+
+    const currentTime = (new Date()).valueOf();
+    const prsByAgeAscending: PullRequestSummary[] = prs
+        .map(pr => summarizePr(pr, currentTime))
+        .sort((prA, prB) => prA.age - prB.age);
+
+    const hasNewPr = prsByAgeAscending[0].age < newPrThreshold;
+
+    return {
+        prs: prsByAgeAscending,
+        newPr: hasNewPr ? prsByAgeAscending[0] : null
+    }
+}
+
 
 async function transmitPulseToIot(pulseDef: PulseDefinition, dataAPI: IotData) {
     let params: IotData.PublishRequest = {
@@ -101,5 +174,8 @@ async function transmitResultToIot(result: boolean, dataAPI: IotData): Promise<a
 }
 
 export {
-    handler
+    handler,
+    getPrData,
+    getAzureApiConnection,
+    getPulsePattern
 };
--- src/test.ts
+++ src/test.ts
@@ -1,2 +1,18 @@
+import { getPrData, getAzureApiConnection, getPulsePattern } from './index';
+import { PullRequestStatus } from 'azure-devops-node-api/interfaces/GitInterfaces';
 
-import { handler } from './index';
+(async() => {
+    try {
+        const token = '';
+        const connection = await getAzureApiConnection(token);
+        const prData = await getPrData(connection, 100, {
+        });
+        console.log(JSON.stringify(prData.prs.map(pr => pr.author)));
+
+        const pulsePattern = await getPulsePattern(token, 1000);
+        console.log(JSON.stringify(pulsePattern));
+    } catch (e) {
+        console.log("ERR");
+        console.log(e);
+    }
+})();
